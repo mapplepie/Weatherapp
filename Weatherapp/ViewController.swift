f2142020ec97712361b3181a9331dedebe3aa823
Mai Uchida <mai.uchida1119@gmail.com>
2021-11-14 13:53:41 +0900

    Initial Commit

---
+++ Weatherapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weatherapp
+//
+//  Created by Mai Uchida on 2021/11/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
